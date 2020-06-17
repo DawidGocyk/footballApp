df0676040f6df8c6836b9c353044b6e4dd3102de
Dawid Gocyk <dawid.gocyk@accenture.com>
2020-06-17 10:15:51 +0200

    Task-1 refactor event details component

--- football-app/src/app/events/event-details.component.ts
+++ football-app/src/app/events/event-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   template: `
+  <div class="container">
   <div>
     <h2>Match Info</h2>
     <span>GameWeek: {{match.match.matchday}}</span>
@@ -15,22 +16,51 @@ import { ActivatedRoute } from '@angular/router';
 </div>
   <div>
     <h2>Head to head info</h2>
-    <span>Numer Of Matches: {{match.head2head.numberOfMatches}}</span>
+    <table class="table table-striped">
+  <thead>
+  <tr>
+    <th scope="col">#</th>
+    <th scope="col">Wins</th>
+    <th scope="col">Draw</th>
+    <th scope="col">Loss</th>
+  </tr>
+  </thead>
+  <tbody>
+    <tr *ngFor="let team of teams">
+    <th scope="row">{{team.name}}</th>
+    <td>{{ team.wins | number }}</td>
+    <td>{{ team.draws | number }}</td>
+    <td>{{ team.losses | number }}</td>
+    </tr>
+  </tbody>
+</table>
+
+
+    <!-- <span>Numer Of Matches: {{match.head2head.numberOfMatches}}</span>
     <div>
      {{match.match.homeTeam.name}}
     </div>
-    <div>Away Team : {{match.match.awayTeam.name}}</div>
-    <span>Stadium: {{match.match.venue}}</span>
+    <div>{{match.match.awayTeam.name}}</div> -->
+</div>
+<button (click)="backToMatches()">back</button>
 </div>
   `
 })
 
 export class EventDetailsComponent implements OnInit {
   match
-  constructor(private eventService: EventService, private route: ActivatedRoute) { }
+  teams = []
+  constructor(private eventService: EventService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.match = this.route.snapshot.data['event']
     console.log('mathc = ', this.match)
+    this.teams.push(this.match.head2head.homeTeam)
+    this.teams.push(this.match.head2head.awayTeam)
+    console.log(this.teams)
+  }
+  backToMatches() {
+    this.router.navigate([''])
+
   }
 }
